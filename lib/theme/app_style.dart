cf2fdb0b43df3e6585027d07d5c9c8fe5d464631
Charles Ifoegbu <charlesifoegbu@gmail.com>
2024-07-25 15:59:20 +0100

    dynamic app signer

--- lib/theme/app_style.dart
+++ lib/theme/app_style.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:tezda_task/utils/app_functions.dart';
 
 class AppStyle {
-  static TextStyle txtQuicksand = GoogleFonts.quicksand(
+  static TextStyle txtQuicksand = TextStyle(
+    fontFamily: 'Quicksand',
     fontSize: 14.0.dynFont,
     fontWeight: FontWeight.w700,
   );
